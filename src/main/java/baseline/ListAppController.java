c1a7dae91f9bae45f3063d037d6ab143b9553fa6
EmanuelPadro <emanuelpadro@knights.ucf.edu>
2021-10-25 01:04:58 -0400

    Added controller

---
+++ src/main/java/baseline/ListAppController.java
@@ -0,0 +1,109 @@
+package baseline;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ListAppController {
+
+    @FXML
+    private Button addList;
+
+    @FXML
+    private Button AddTask;
+
+    @FXML
+    private Button AllTasks;
+
+    @FXML
+    private Button CompleteTasks;
+
+    @FXML
+    private Button DisplayAllList;
+
+    @FXML
+    private Button EditTask;
+
+    @FXML
+    private Button EditTitle;
+
+    @FXML
+    private TextField FileLocationTextbox;
+
+    @FXML
+    private Button ImcompleteTasks;
+
+    @FXML
+    private Button MarkComplete;
+
+    @FXML
+    private Button RemoveList;
+
+    @FXML
+    private Button RemoveTask;
+
+    @FXML
+    private Button saveList;
+
+    @FXML
+    void saveListClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addListPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addTaskClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void displayCompleteTasks(ActionEvent event) {
+
+    }
+
+    @FXML
+    void displayIncompleteTasks(ActionEvent event) {
+
+    }
+
+    @FXML
+    void displayLists(ActionEvent event) {
+
+    }
+
+    @FXML
+    void displayTasks(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editTaskClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editTitleClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void markCompleteClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void removeListClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void removeTaskClicked(ActionEvent event) {
+
+    }
+
+}
