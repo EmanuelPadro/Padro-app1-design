b3e66c44a9632b18c23f962dd4733bb366f3ad1a
EmanuelPadro <emanuelpadro@knights.ucf.edu>
2021-10-25 17:09:54 -0400

    Forgot some puml changes

--- src/main/java/baseline/ListAppController.java
+++ src/main/java/baseline/ListAppController.java
@@ -2,7 +2,7 @@ package baseline;
 
 /*
  *  UCF COP3330 Summer 2021 Application Assignment 1 Solution
- *  Copyright 2021 first_name last_name
+ *  Copyright 2021 Emanuel Padro
  */
 
 import javafx.event.ActionEvent;
