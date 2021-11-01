cf7b1dc29aeb3eb8663d2e40b21f7324f0948590
EmanuelPadro <emanuelpadro@knights.ucf.edu>
2021-11-01 18:49:24 -0400

    New plan inserted

---
+++ src/main/java/baseline/Task.java
@@ -0,0 +1,12 @@
+package baseline;
+
+public class Task {
+    String description;
+    String dueDate;
+
+    Task(String inputDescription, String inputDueDate){
+        description = inputDescription;
+        dueDate = inputDueDate;
+    }
+    //the new plan is to make an object that constructs a task and have an array of tasks
+}
